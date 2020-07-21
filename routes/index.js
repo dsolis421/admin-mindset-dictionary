da136bd8a5d01414b4c2797b49d237aa3624d18a
dsolis421 <danny@march76.com>
2020-07-21 18:05:55 -0500

    added initial routes for home page and dictionaryalphalist

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const dictionaryController = require('../controllers/dictionaryController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Admin' });
 });
+
+router.get('/dictionary/:letter', dictionaryController.getDictionaryLetter);
 
 module.exports = router;
