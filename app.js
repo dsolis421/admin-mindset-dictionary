717c1369a15a580411e70be65b2107fe997664fc
dsolis421 <danny@march76.com>
2020-07-21 16:14:29 -0500

    added mongodb connection

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/*put models here*/
+
 var app = express();
 
 // view engine setup
