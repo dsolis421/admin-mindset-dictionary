8cab7916008f7eff19ed2495dee8b2efbaa5dc48
dsolis421 <danny@march76.com>
2020-07-21 18:03:12 -0500

    added mongodb models

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 /*put models here*/
+require('./models/mindsetDictionarySchema');
 
 var app = express();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
