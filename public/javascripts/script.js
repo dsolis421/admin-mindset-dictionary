6ba15840b3525736b7a350515cdaa4b3d1aae424
dsolis421 <danny@march76.com>
2020-07-22 11:55:26 -0500

    added logic for new form open and cancel, submit had button but needs final db post

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,12 @@
+function buildNewTerm() {
+  $('.new-term-container').animate({height: "675px"},"slow","swing");
+}
+
+function clearNewValues() {
+  $('.new-term-form > input, .new-term-form > textarea').val('');
+}
+
+
 $(document).ready(function(){
 
   $('#letter-search-go').click(function() {
@@ -5,6 +14,27 @@ $(document).ready(function(){
     var $quick = "/dictionary/" + $letter;
     console.log('selected letter '+ $quick);
     window.location.href = $quick;
+  });
+
+  $('.edit-photo-term').click(function() {
+    console.log('editing this term')
+  });
+
+  $('#add-new-term').click(function() {
+    console.log('adding a new term');
+    clearNewValues();
+    buildNewTerm();
+  });
+
+  $('.new-term-submit').click(function() {
+    console.log('submit a new term');
+    clearNewValues();
+  });
+
+  $('.new-term-cancel').click(function() {
+    console.log('adding a new term');
+    clearNewValues();
+    $('.new-term-container').animate({height: "0px"},"slow","swing");
   });
 
 });
