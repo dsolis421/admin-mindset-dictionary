4f77d1ffdef266426ef30343da42d40b01357650
dsolis421 <danny@march76.com>
2020-07-22 19:40:29 -0500

    added post for click on new term form submit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-function buildNewTerm() {
+function expandNewTermForm() {
   $('.new-term-container').animate({height: "675px"},"slow","swing");
 }
 
@@ -6,8 +6,28 @@ function clearNewValues() {
   $('.new-term-form > input, .new-term-form > textarea').val('');
 }
 
+function postNewTerm(newterm) {
+  $.post("/dictionary", newterm);
+}
+
+function buildNewTerm() {
+  var newtermdata = {};
+  var $lettercategory = $('#new-dictionary-term').val().charAt(0).toLowerCase();
+  var $newtermquick = $('#new-dictionary-term').val().replace(/\)/g,"").replace(/\(/g,"").replace(/ /g,"-").toLowerCase();
+  var $synonyms = $('#new-term-synonyms').val().replace(/, /g,",").split(",");
+  var $relatedterms = $('#new-term-related').val().replace(/, /g,",").split(",");
+  newtermdata.term = $('#new-dictionary-term').val();
+  newtermdata.termquick = $newtermquick;
+  newtermdata.definition = $('#new-term-def').val();
+  newtermdata.synonyms = $synonyms;
+  newtermdata.relatedterms = $relatedterms;
+  newtermdata.lettercategory = $lettercategory;
+  console.log(newtermdata);
+  postNewTerm(newtermdata);
+}
 
 $(document).ready(function(){
+  clearNewValues();
 
   $('#letter-search-go').click(function() {
     var $letter = $('#letter').val()
@@ -23,12 +43,12 @@ $(document).ready(function(){
   $('#add-new-term').click(function() {
     console.log('adding a new term');
     clearNewValues();
-    buildNewTerm();
+    expandNewTermForm();
   });
 
   $('.new-term-submit').click(function() {
     console.log('submit a new term');
-    clearNewValues();
+    buildNewTerm();
   });
 
   $('.new-term-cancel').click(function() {
@@ -36,5 +56,4 @@ $(document).ready(function(){
     clearNewValues();
     $('.new-term-container').animate({height: "0px"},"slow","swing");
   });
-
 });
