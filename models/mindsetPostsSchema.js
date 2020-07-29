b305b6e14dad83d4a2a40b207a721835f57899e4
dsolis421 <danny@march76.com>
2020-07-29 10:14:45 -0500

    added schema for blog posts - mindsetPostsSchema

---
+++ models/mindsetPostsSchema.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+
+const postsSubText = new mongoose.Schema({
+  'postsubheader' : {
+    type : String,
+    required : false
+  },
+  'postsubimage' : {
+    type : String,
+    required : false
+  },
+  'postsubtext' : {
+    type : String,
+    required : true
+  },
+  'postsuborder' : {
+    type : Number,
+    required : true
+  }
+});
+
+const postsSchema = new mongoose.Schema({
+  'posttitle' : {
+    type: String,
+    required: true
+  },
+  'postdate' : {
+    type : Date,
+    required : false
+  },
+  'posttopic' : {
+    type : String,
+    required : true
+  },
+  'postquick' : {
+    type: String,
+    required: true
+  },
+  'postsummary' : {
+    type : String,
+    required : true
+  },
+  'postcontent' : {
+    type : [postsSubText],
+    required : true
+  },
+  'postimage' : {
+    type : String,
+    required : true
+  },
+  'posttnail' : {
+    type: String,
+    required : true
+  },
+  'showpost' : {
+    type : String,
+    required : true
+  }
+});
+
+module.exports = mongoose.model('mindsetposts', postsSchema);
